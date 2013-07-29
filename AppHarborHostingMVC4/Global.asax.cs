000a6c18d2537bba9f3e2960c396fae13db79d82
Taha Azab <tehadev@gmail.com>
2013-07-29 05:20:21 -0400

    added new comment to global.asx

--- AppHarborHostingMVC4/Global.asax.cs
+++ AppHarborHostingMVC4/Global.asax.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +11,7 @@ namespace AppHarborHostingMVC4
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
-
+// added new comment line
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
